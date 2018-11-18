f336a9f5e81ec762c0cc70c62738e9b4719fad02
arjunchowdaryd <arjunchowdary.d@outlook.com>
2018-11-18 20:03:10 +0530

    This is my angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'second-project';
+  loadedFeature='recipe';
+ 
+  onNavigate(feature:string){
+   this.loadedFeature=feature;
+ }
+ title="nagarjuna";
+
+
+
+  //   allowServer = false;
+//   server:string="no server created";
+//   ServerName:string='text';
+//   name:string = 'arjun';
+//   age:number = 22;
+//   height:number =5.11;
+//   color:string ='lite-brown';
+//   text:string ="click the button";
+//  constructor(){
+//    setTimeout(() => {
+//      this.allowServer=true;
+//    },2000);
+//  }
+//  onCreateServer(){
+//    this.server="server created! name is"+this.ServerName;
+//  }
+//  onServerName(event:Event){
+//  this.ServerName=(<HTMLInputElement>event.target).value;
+
+//  }
+
 }
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient }  from '../shared/Ingredient.model';
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients:{name:string,amount:number}={
+       name:"apple",
+       amount:5,
+  }
+ 
+  // ingredients:Ingredient[] =[
+  //   new Ingredient('apples',5),
+  //   new Ingredient('tomatoes',10),
+    
+  // ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
